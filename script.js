0e952b1178aac3b8571f739853b4d43e3e51c8b4
s111ew <167631312+s111ew@users.noreply.github.com>
2024-06-03 19:40:35 +0100

    Install node dependencies and define player stats object

---
+++ script.js
@@ -0,0 +1,16 @@
+//Knights of Purgatory
+//--------------------
+
+
+const prompt = require("prompt-sync")();
+
+let playerStats = {};
+
+playerStats.name = "";
+playerStats.gender = "";
+playerStats.luck = "";
+playerStats.faith = "";
+playerStats.strength = "";
+playerStats.equipped = "";
+
+console.log(playerStats);
